3f4cb2fe5a6528725a991eeac8d00482f66cfff4
Travis Brewer <t.brewer.co@gmail.com>
2021-06-10 13:26:28 -0600

    initial configuration

---
+++ server.js
@@ -0,0 +1,23 @@
+// Deopendencies
+const express = require("express");
+const mongoose = require('mongoose');
+const app = express();
+require("dotenv").config();
+
+// Database Configuration
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
+});
+
+// Database Connection Error / Success
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// Listener
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
