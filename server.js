374de617f038ce338beea71e954c13ed4cafd7be
Travis Brewer <t.brewer.co@gmail.com>
2021-06-10 13:29:40 -0600

    configure body parser middleware

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
+// Middleware
+// Body parser middleware: give us access to req.body
+app.use(express.urlencoded({ extended: true }));
+
+// Routes / Controllers
+const userController = require("./controllers/users");
+app.use("/users", userController);
+
 // Listener
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
